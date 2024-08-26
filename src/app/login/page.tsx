f518e59834eeb8ff99e81924ddb24874939040bd
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-08-26 19:28:50 -0400

    disable button on awaiting the post request

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,41 @@
 "use client";
 
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const router = useRouter();
+
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    setIsLoading(true);
 
     const formData = new FormData(event.currentTarget);
     const password = formData.get("password");
 
-    const response = await fetch('/api/auth/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ password }),
-    })
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ password }),
+      })
+      
+      if (response.ok) {
+        setHasError(false);
+        router.push("/");
+      } else {
+        throw new Error('Login failed');
+      }
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
 
-    console.log(response);
+    setIsLoading(false);
   }
 
   return (
@@ -25,6 +44,7 @@ export default function LoginPage() {
         <form onSubmit={handleSubmit}>
           <div className="my-8 text-center">
             <h1 className="text-2xl font-bold"> Welcome to Isabelles Blog! </h1>
+            {hasError ? <p className="text-red-900 text-sm"> Please try again. </p> : <p className="text-sm"> Please enter the password below. </p> }
           </div>
 
           <div className="grid gap-4">
@@ -32,7 +52,7 @@ export default function LoginPage() {
               Password:
               <input className="block w-full p-2 border-2 border-gray-200 rounded-md text-sm shadow-sm focus:border-red-900" type="password" name="password" required />
             </label>
-            <button className="block w-full p-2 rounded bg-indigo-600 text-white" type="submit"> Submit </button>
+            <button className="block w-full p-2 rounded bg-indigo-600 text-white" type="submit" disabled={isLoading}> {isLoading ? 'Submitting...' : 'Submit'} </button>
           </div>
         </form>
       </div>
