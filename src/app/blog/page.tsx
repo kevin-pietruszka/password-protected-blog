efb8568d733bb5c752f06a10e54570e4eda4940d
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-08-29 10:25:46 -0400

    prototype blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,12 @@
 export default function Blog() {
   return (
-    <main>
+    <main className="max-w-5xl mx-auto">
+      <section id="hero">
+        <div id="hero-content" className="bg-lilac flex flex-col items-center justify-center py-16">
+          <h1 className="text-2xl md:text-4xl"> {"Welcome to Isabelle's Blog!"} </h1>
+        </div>
+      </section>
+      <section> Blog List </section>
     </main>
   );
 }
