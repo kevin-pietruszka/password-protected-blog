77f58df6c0868a3f0d63fb6a5db6f10b28d8cfd0
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-10-03 13:26:44 -0400

    change editor to use css module

--- src/components/blog-editor.tsx
+++ src/components/blog-editor.tsx
@@ -2,7 +2,7 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/shadcn";
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/shadcn/style.css";
-import "@/styles/editor.css";
+import "@/styles/editor.module.css";
 
 import { useTheme } from "next-themes";
 
