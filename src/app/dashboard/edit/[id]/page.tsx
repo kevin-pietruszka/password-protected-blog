00d1ab886de574266fc21b1ff7088380774d60d0
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-10-06 17:41:52 -0400

    fetch blog on edit page

--- src/app/dashboard/edit/[id]/page.tsx
+++ src/app/dashboard/edit/[id]/page.tsx
@@ -1,3 +1,32 @@
-export default function EditBlogPage() {
-  return (<></>);
+import { fetchBlogById } from "@/lib/data-actions";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+export default async function EditBlogPage({ params }: { params: { id: string } }) {
+
+  const id = params.id;
+  const blog = await fetchBlogById(id);
+
+  return (
+    <section>
+      <Breadcrumb className="mb-8">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/dashboard"> Dashboard </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage> Edit </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <h1> {blog.title} </h1>
+    </section>
+  );
 }
