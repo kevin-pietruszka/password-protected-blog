07207af624b8986497f9477a57bc44620ee67515
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-10-06 21:19:43 -0400

    create form for updating

---
+++ src/components/update-blog-form.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { Blog, ClientBlogSchema } from "@/lib/definitions";
+
+import dynamic from "next/dynamic";
+import { updateBlog } from "@/lib/data-actions";
+
+const Editor = dynamic(() => import("@/components/blog-editor"), { ssr: false });
+
+export default function UpdateBlogForm({id, blog}: {id: string, blog:Blog}) {
+
+  const form = useForm<z.infer<typeof ClientBlogSchema>>({
+    resolver: zodResolver(ClientBlogSchema),
+    defaultValues: {
+      title: blog.title,
+      date: blog.date,
+      status: blog.status,
+      content: Promise.resolve(blog.content),
+    }
+  });
+
+  async function onSubmit(values: z.infer<typeof ClientBlogSchema>) {
+    const serverData: Blog = {
+      ...values,
+      content: await values.content,
+    };
+       
+    try {
+      await updateBlog(id, serverData);
+    } catch (error) {
+      alert("Error uploading, please try again.")
+    }
+  }
+
+  return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Title </FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Title" className="w-[240px]" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel> Date </FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Upload status </FormLabel>
+                <FormControl>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <SelectTrigger className="w-[240px]">
+                      <SelectValue placeholder="Select a role" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Unlisted">Unlisted</SelectItem>
+                      <SelectItem value="Public">Public</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Content </FormLabel>
+                <FormControl>
+                  <Editor
+                    onContentChange={field.onChange}
+                    initialValue={blog.content}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit"> {"Create blog"} </Button>
+        </form>
+      </Form>
+  );
+}
