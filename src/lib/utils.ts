4f3d27d00221bbb7ee4828726bb84b097c563c6f
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-10-08 18:54:11 -0400

    function to make ids for blogs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function titleToId(title: string) {
+  return title
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, '');
+}
