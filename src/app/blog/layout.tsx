5d6db5c0178c1d3cb1cfd5c5661a2761696d8ec5
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-10-09 15:42:56 -0400

    add link to home page from blog

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -3,12 +3,15 @@ import { SignOutButton } from "@/components/auth/signout-button";
 import { ThemeButton } from "@/components/theme-button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Menu } from "lucide-react";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main>
       <header className="flex justify-between items-center max-w-6xl mx-auto px-4 py-8">
-        <h1 className="font-bold text-4xl"> {"Isabelle's Blog"} </h1>
+        <Link href="/">
+          <h1 className="font-bold text-4xl"> {"Isabelle's Blog"} </h1>
+        </Link>
         <nav className="flex items-center justify-center gap-2">
           <div className="hidden md:flex items-center gap-2">
             <DashboardButton />
